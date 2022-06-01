dbf8e5d9aa8bc6ba0f845bde5919dbc4b82fc62e
Siddharth C Prabhu <scp220@lehigh.edu>
2022-06-01 12:11:39 -0400

    updated class

--- GenerateData.py
+++ GenerateData.py
@@ -2,14 +2,12 @@ from dataclasses import dataclass, field
 import numpy as np
 np.random.seed(10)
 
-from typing import Optional
+from typing import ClassVar
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-# add features : change initial conditions, solution should automatically change
-# noise should also (property). add timespam, initial in init
 # add checks on the args, lenght of the inputs variables, etc
-# 
+
 @dataclass
 class DynamicModel():
 
@@ -17,13 +15,12 @@ class DynamicModel():
     initial_condition : np.ndarray 
     time_span : np.ndarray 
 
-    model_dict : dict = field(init = False)
-    solution_flag : bool = field(init = False, default = False)
+    _model_dict : ClassVar 
 
     def __post_init__(self):
-        self.model_dict = {"kinetic_simple" : DynamicModel.kinetic_simple,
+        self._model_dict = {"kinetic_simple" : DynamicModel.kinetic_simple,
                             "kinetic_kosir" : DynamicModel.kinetic_kosir}
-        assert self.model in self.model_dict, "Dynamic model is not defined yet"
+        assert self.model in self._model_dict, "Dynamic model is not defined yet"
 
     @staticmethod
     def kinetic_simple(x, t, *args):
@@ -31,43 +28,50 @@ class DynamicModel():
         # k1 = 1, kr1 = 0.5, k2 = 2, kr2 = 1
         k1, kr1, k2, kr2 = args
         
-        return [-k1*x[0]*x[1] + kr1*x[2],
+        return np.array([-k1*x[0]*x[1] + kr1*x[2],
                 -k1*x[0]*x[1] + (kr1 + k2)*x[2],
                 k1*x[0]*x[1] - (kr1 + k2)*x[2],
-                k2*x[2]]
+                k2*x[2]])
     
     @staticmethod
-    def kinetic_kosir(x, t):
+    def kinetic_kosir(x, t, *args):
 
-        return [-15.693*x[0] + 5.743*x[2] + 1.534*x[3],
+        return np.array([-15.693*x[0] + 5.743*x[2] + 1.534*x[3],
             8.566*x[0],
             1.191*x[0] - 5.743*x[2],
-            10.219*x[0] - 1.535*x[3]]
+            10.219*x[0] - 1.535*x[3]])
 
     # forward simulates the chosen model using scipy odeint
     def integrate(self, model_args: tuple, **odeint_kwargs):
         
-        self.solution = odeint(self.model_dict[self.model], self.initial_condition, 
-                        self.time_span, args = model_args, **odeint_kwargs)
-        self.solution_flag = True
+        self._solution_flag = True
+        self._model_args = model_args
+        self.solution = odeint(self._model_dict[self.model], self.initial_condition, 
+                        self.time_span, args = self._model_args, **odeint_kwargs)
 
-        return self.solution
+        return self.solution 
 
     # plots the integrated solution with respect to time
-    def plot(self):
-        assert self.solution_flag, "Integrate the model before calling this method"
+    def plot(self, **kwargs):
+        assert self.solution, "Integrate the model before calling this method"
         
-        plt.plot(self.time_span, self.solution)
+        plt.plot(self.time_span, self.solution, **kwargs)
         plt.show()
 
     # calculates the actual derivative of the model
-    def actual_derivatives(self):
-        assert self.solution_flag, "Integrate the model before calling this method"
-        
+    @property
+    def actual_derivative(self):
+        assert self.solution, "Integrate the model before calling this method"
+        return np.vectorize(self._model_dict[self.model], signature = "(m),(n),(k)->(m)")(self.solution, self.time_span, self._model_args)
 
+    # calculates the approximate derivative using finite difference
+    def approx_derivative(self):
+        # (self.solution[1:, :] -2*self.solution[] + self.solution[:-1, :])/(self.time_span[1:] - self.time_span[:-1])**2
+        pass
+    
     # adds gaussian noise the the datapoints
     def add_noise(self, mean, variance, mulitplicative = "False"):
-        assert self.solution_flag, "Integrate the model before calling this method"
+        assert self.solution, "Integrate the model before calling this method"
 
         if multiplicative:
             self.solution_noise = self.solution * (1 + np.random.normal(mean, variance, size = self.solution.shape))
@@ -77,10 +81,16 @@ class DynamicModel():
         return self.solution_noise
 
 
+
 if __name__ == "__main__":
 
     t_span = np.arange(0, 10, 0.1)
     x_init = np.array([1, 2, 3, 4])
     model = DynamicModel("kinetic_kosir", x_init, t_span)
     model.integrate(())
-    model.plot()
+    # model.plot()
+
+    model.initial_condition = np.array([5, 6, 7, 8])
+    model.integrate(()) # call integrate eveytime data is changed
+    
+    print(model.actual_derivative)
