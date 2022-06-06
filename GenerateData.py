c505ab65f4e298b75693b41550becc66e0b8df42
Siddharth C Prabhu <scp220@lehigh.edu>
2022-06-06 11:36:33 -0400

    Updated multiple experiments

--- GenerateData.py
+++ GenerateData.py
@@ -2,18 +2,19 @@ from dataclasses import dataclass, field
 import numpy as np
 np.random.seed(10)
 
+import pickle
 from typing import ClassVar
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
-
 import pysindy as ps
 
 @dataclass
 class DynamicModel():
 
     model : str 
-    initial_condition : np.ndarray 
-    time_span : np.ndarray 
+    time_span : np.ndarray
+    initial_condition : list  
+    n_expt : int = field(default = 1)
 
     _model_dict : ClassVar 
     _solution_flag : bool = field(init = False, default = False)
@@ -22,8 +23,13 @@ class DynamicModel():
         self._model_dict = {"kinetic_simple" : {"function" : DynamicModel.kinetic_simple, "n_states" : 4},
                             "kinetic_kosir" : {"function" : DynamicModel.kinetic_kosir, "n_states" : 4}}
         assert self.model in self._model_dict, "Dynamic model is not defined yet"
-        assert len(self.initial_condition) == self._model_dict[self.model]["n_states"], "Incorrect number of states"
         
+        if not len(self.initial_condition):
+            self.initial_condition = [np.random.uniform(0, 20, size = self._model_dict[self.model]["n_states"]) for _ in range(self.n_expt)]
+        else:
+            assert len(self.initial_condition[-1]) == self._model_dict[self.model]["n_states"], "Incorrect number of states"
+            assert len(self.initial_condition) == len(self.n_expt), "Initial conditions should match the number of experiments"
+
         self.model = self._model_dict[self.model]["function"]
 
     @staticmethod
@@ -50,8 +56,8 @@ class DynamicModel():
         
         self._solution_flag = True
         self._model_args = model_args
-        self.solution = odeint(self.model, self.initial_condition, 
-                        self.time_span, args = self._model_args, **odeint_kwargs)
+        self.solution = [odeint(self.model, xi, 
+                        self.time_span, args = self._model_args, **odeint_kwargs) for xi in self.initial_condition]
 
         return self.solution 
 
@@ -69,48 +75,48 @@ class DynamicModel():
     @property
     def actual_derivative(self):
         assert self._solution_flag, "Integrate the model before calling this method"
-        return np.vectorize(self.model, signature = "(m),(n),(k)->(m)")(self.solution, self.time_span, self._model_args)
+        return [np.vectorize(self.model, signature = "(m),(n),(k)->(m)")(xi, self.time_span, self._model_args) for 
+                xi in self.solution]
 
     # calculates the approximate derivative using finite difference
     @property
     def approx_derivative(self):
         assert self._solution_flag, "Integrate the model before calling the method"
         
-        middle = np.zeros_like(self.solution)
+        # middle = np.zeros_like(self.solution)
         # second order central difference on the middle elements
-        middle[1:-1] = (self.solution[2:] -2*self.solution[1:-1] + self.solution[:-2])/((self.time_span[2:] - self.time_span[1:-1])**2).reshape(-1, 1)
+        # middle[1:-1] = (self.solution[2:] -2*self.solution[1:-1] + self.solution[:-2])/((self.time_span[2:] - self.time_span[1:-1])**2).reshape(-1, 1)
         # second order forward difference on the first element
-        middle[0] = (self.solution[2] - 2*self.solution[1] + self.solution[0])/(self.time_span[1] - self.time_span[0])**2
+        # middle[0] = (self.solution[2] - 2*self.solution[1] + self.solution[0])/(self.time_span[1] - self.time_span[0])**2
         # second order backward difference on the last element
-        middle[-1] = (self.solution[-1] - 2*self.solution[-2] + self.solution[-3])/(self.time_span[-1] - self.time_span[0])**2
+        # middle[-1] = (self.solution[-1] - 2*self.solution[-2] + self.solution[-3])/(self.time_span[-1] - self.time_span[0])**2
         
-        return middle
+        return [ps.FiniteDifference()._differentiate(xi, self.time_span) for xi in self.solution]
     
     # adds gaussian noise the the datapoints
     @staticmethod
-    def add_noise(data : np.ndarray, mean : float = 0, variance : float  = 0.1, multiplicative = "False"):
+    def add_noise(data : list, mean : float = 0, variance : float  = 0.1, multiplicative = "False"):
         if multiplicative:
-            return data * (1 + np.random.normal(mean, variance, size = data.shape))
+            return [value * (1 + np.random.normal(mean, variance, size = value.shape)) for value in data]
         else:
-            return data + np.random.normal(mean, variance, shape = data.shape)
+            return [value + np.random.normal(mean, variance, shape = value.shape) for value in data]
 
-
+    @staticmethod
+    def save_data(data : dict, path : str):
+        
+        with open(path, "wb") as file:
+            pickle.dump(data, file)
 
 if __name__ == "__main__":
 
     t_span = np.arange(0, 10, 0.1)
-    x_init = np.array([1, 2, 3, 4])
-    model = DynamicModel("kinetic_kosir", x_init, t_span)
+    # x_init = np.array([1, 2, 3, 4])
+    model = DynamicModel("kinetic_kosir", t_span, [], 2)
     solution = model.integrate(())
     # model.plot(solution, t_span, "Time", "Concentration", ["A", "B", "C", "D"])
 
-    model.initial_condition = np.array([5, 6, 7, 8])
-    model.integrate(()) # call integrate eveytime data is changed
-    
     print(model.actual_derivative)
     print("--"*20)
     print(model.add_noise(model.actual_derivative))
     print("--"*20)
-    print(solution.shape)
     
-    print(np.isclose(ps.FiniteDifference()._differentiate(solution, t_span), model.approx_derivative, atol = 10**-5))
